593a367765967ecc843cdb2fb0d71ed82d6eef60
KhojaHoldingCorp <rahimk@khojacorp.com>
2017-02-13 16:43:33 -0800

    Update install_monero_miner.sh

--- install_monero_miner.sh
+++ install_monero_miner.sh
@@ -62,50 +62,39 @@ fi
 echo "Installing Monero Miner"
   
 # Update & Upgrade Apt
-
 sudo apt-get -y update && sudo apt-get -y upgrade
 
 # get git to install it
-
 sudo apt-get install git wget build-essential autotools-dev libcurl3 automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++
 
 # download latest version
-
 git clone https://github.com/wolf9466/cpuminer-multi
 
 # not as fast I don't know why
 # git clone https://github.com/tpruvot/cpuminer-multi
-
 cd cpuminer-multi/
 
 # compile
-
 ./autogen.sh
-
 CFLAGS="-march=native" ./configure
-
 make
 
 #install
-
 sudo make install
 
 # return to install dir
-
 cd ..
 
 # Copy Service File
-
 sudo /bin/bash -c "cp $(pwd -P)/monero-miner.service /lib/systemd/system/"
 
-# Update SystemD file
-
-sudo sed -e "s/{walletaddress}/$WALLET/" -e "s/{mineaddress}/$MINEHOST/"  /lib/systemd/system/monero-miner.service
+# Update Systemd file
+sudo sed -i "s/walletaddress/$WALLET/" /lib/systemd/system/monero-miner.service
+sudo sed -i "s/mineaddress/$MINEHOST/" /lib/systemd/system/monero-miner.service
 
 # Reload Systemd Services
 sudo systemctl daemon-reload
 
 # Enable and Start the Miner
-
 sudo systemctl enable monero-miner.service
 sudo systemctl start monero-miner.service
